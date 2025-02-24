3d0e6268b3c1021928a650863800972e059aa365
SIGA-App <jessicavtc@gmail.com>
2025-02-24 11:03:03 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
     # Armazene o banco de dados na pasta 'instance' para persistência
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'instance', 'new.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
