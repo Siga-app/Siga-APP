f17658e0cee34ab0ae5458909965299e4ac0fa45
SIGA-App <jessicavtc@gmail.com>
2025-02-24 12:46:16 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
-    # Usar caminho absoluto do volume persistente no Render
+    # Usar caminho absoluto para o volume persistente no Render.
+    # Certifique-se de que o volume esteja montado em /app/instance e que o arquivo seja new.db.
     SQLALCHEMY_DATABASE_URI = 'sqlite:////app/instance/new.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
