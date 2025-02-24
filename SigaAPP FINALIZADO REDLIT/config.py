298e52c9c24a50338dbd53fdbc2a1a374b63f8af
SIGA-App <jessicavtc@gmail.com>
2025-02-23 22:02:33 -0300

    Delete SigaAPP FINALIZADO REDLIT directory

--- SigaAPP FINALIZADO REDLIT/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
-    # Use um nome diferente para forçar a criação de um novo DB
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///new.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
